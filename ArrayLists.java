f5c92235b8c665f09ac46b5d9cb371789eb5d6ee
Vijaya Nandini <vijayanandinimusale@gmail.com>
2026-06-22 00:24:53 +0530

    first commit

---
+++ ArrayLists.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class ArrayLists{
+    public static void main(String[] args) {
+        List<Integer> l1 = new ArrayList<>();
+        for(int i=0;i<5;i++){
+        l1.add(i);
+
+        }
+        System.out.println(l1);
+    }
+}
